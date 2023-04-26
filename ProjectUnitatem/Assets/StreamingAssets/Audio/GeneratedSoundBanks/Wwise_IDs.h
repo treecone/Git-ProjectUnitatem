de3ec55f49c63aa1d72534e75a3597019587bd06
BPMPRODUCTIONS <108146652+BPMPRODUCTIONS@users.noreply.github.com>
2023-04-26 14:35:02 -0700

    Wwise Organization

--- ProjectUnitatem/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ ProjectUnitatem/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,74 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_BGM_PLAYLIST_1 = 486851409U;
+        static const AkUniqueID DEATH = 779278001U;
+        static const AkUniqueID ENEMY_ATTACK = 1781417190U;
+        static const AkUniqueID MOVE = 3011204530U;
+        static const AkUniqueID PAUSE_MENU = 3422541661U;
+        static const AkUniqueID PLAY_BGM = 3126765036U;
+        static const AkUniqueID PLAYER_ATTACK = 2824512041U;
+        static const AkUniqueID RESUME_GAME = 1565052233U;
+        static const AkUniqueID SET_8BITSWITCH = 3901001571U;
+        static const AkUniqueID SET_GUITARSWITCH = 4003498308U;
+        static const AkUniqueID SET_VIOLINSWITCH = 1399716893U;
+        static const AkUniqueID START_GAME = 1114964412U;
+        static const AkUniqueID STOP_ALL = 452547817U;
+        static const AkUniqueID VICTORY_STINGER = 2155888594U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace PHASES
+        {
+            static const AkUniqueID GROUP = 3630029033U;
+
+            namespace STATE
+            {
+                static const AkUniqueID COMBAT = 2764240573U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TRANSITION = 1865857008U;
+            } // namespace STATE
+        } // namespace PHASES
+
+        namespace PLAYER_LIFE
+        {
+            static const AkUniqueID GROUP = 3762137787U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ALIVE = 655265632U;
+                static const AkUniqueID DEAD = 2044049779U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace PLAYER_LIFE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace MUSICSWITCH
+        {
+            static const AkUniqueID GROUP = 1445037870U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GUITAR = 3232836819U;
+                static const AkUniqueID RETRO = 3496907731U;
+                static const AkUniqueID VIOLIN = 3063529226U;
+            } // namespace SWITCH
+        } // namespace MUSICSWITCH
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID BGM_CONTROLLER = 3230961072U;
+        static const AkUniqueID DAMAGETOBOSS = 1049310662U;
+        static const AkUniqueID DISTORTION = 1517463400U;
+        static const AkUniqueID PAUSEFILTER = 1201445347U;
+        static const AkUniqueID PLAYER_HEALTH = 215992295U;
+        static const AkUniqueID SCORE = 2398231425U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
@@ -25,7 +91,11 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID BGM_BUS = 1697111475U;
+        static const AkUniqueID ENEMY_ATTACK_BUS = 1664460283U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID PLAYER_ATTACK_BUS = 1578025756U;
+        static const AkUniqueID TRANSITIONS_BUS = 228317785U;
+        static const AkUniqueID UI_BUS = 3600729941U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
