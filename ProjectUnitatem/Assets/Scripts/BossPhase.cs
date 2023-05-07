a84297041a4add6008239c4b1791665058f888b9
SeanLynch717 <sdlgamerguy@gmail.com>
2023-05-07 16:55:16 -0500

    More Boss Moves!!

--- ProjectUnitatem/Assets/Scripts/BossPhase.cs
+++ ProjectUnitatem/Assets/Scripts/BossPhase.cs
@@ -14,6 +14,11 @@ public class BossPhase
         }
     }
 
+
+    public int GetNumMoves()
+    {
+        return _bossMoves.Count;
+    }
     public BossMoveDescription GetMove()
     {
         int random = Random.Range(0, _totalWeight);
--- ProjectUnitatem/Assets/Scripts/BossPhaseManager.cs
+++ ProjectUnitatem/Assets/Scripts/BossPhaseManager.cs
@@ -4,18 +4,14 @@ using UnityEngine;
 
 public class BossPhaseManager : MonoBehaviour
 {
-    private BossPhase[] _bossPhases = new BossPhase[4];
+    private BossPhase[] _bossPhases = new BossPhase[1];
     private BossMoveDescription _currentMove;
     private int _currentPhase = 0;
 
     private BulletManager _bulletManager;
     private GameObject _player;
     private GameObject _boss;
-
-    private int _beamBeatCounter = 0;
-    private int _plopBeatCounter = 0;
-    // How many beats must elapse for a new plop
-    private int _plopPeriod = 2;
+    private bool _enableRepeatedMoves = false;
 
     // Start is called before the first frame update
     void Start()
@@ -28,9 +24,23 @@ public class BossPhaseManager : MonoBehaviour
         _bossPhases[0] = new BossPhase(
             new List<BossMoveDescription>()
             {
-                new BossMoveDescription(ShootInSpiral, 1)
+                new BossMoveDescription(Move_ShootInSpiral, 1),
+                new BossMoveDescription(Move_CrossFireBeams, 1),
+                new BossMoveDescription(Move_CircularBlast, 1),
+                new BossMoveDescription(MOVE_BlowUpOnPlayer, 1),
+                new BossMoveDescription(Move_SpawnFragmentingCirclesAroundBoss, 1),
             }
         );
+
+        // Debug safeguard against only having one move and not enabling repeated moves...
+        // It will crash Unity otherwise.
+        foreach(var bossPhase in _bossPhases)
+        {
+            if(bossPhase.GetNumMoves() == 1)
+            {
+                _enableRepeatedMoves = true;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -42,22 +52,133 @@ public class BossPhaseManager : MonoBehaviour
     {
         if(_currentMove == null || _currentMove.Complete)
         {
+            BossMoveDescription candidateMove;
+            do
+            {
+                candidateMove = _bossPhases[_currentPhase].GetMove();
+            } 
+            while (!_enableRepeatedMoves && candidateMove == _currentMove);
             _currentMove = _bossPhases[_currentPhase].GetMove();
             _currentMove.ExecuteAction();
         }
-        object[] param = new object[] { 2.0f, null };
     }
 
     #region BOSS ACTIONS
-    public void ShootInSpiral(BossMoveDescription desc)
+    public void Move_ShootInSpiral(BossMoveDescription desc)
     {
-        BulletDescription baseDescription = new BulletDescription(BULLET_TYPE.Base, gameObject.transform.position, Quaternion.Euler(0, 0, GetZRotationTowardsObj(_player)));
-        _bulletManager.SpawnFromPool(baseDescription);
-        StartCoroutine(WaitAndMarkComplete(new object[] { 2.0f, desc }));
+        StartCoroutine(SpawnBulletsInSpiral(desc));
     }
+    private IEnumerator SpawnBulletsInSpiral(BossMoveDescription desc)
+    {
+        float angle = Mathf.PI / 8 - 0.1f;
+        for(int i = 0; i < 50; i++)
+        {
+            BulletDescription baseDescription = new BulletDescription(BULLET_TYPE.Base, gameObject.transform.position, Quaternion.Euler(0, 0, Mathf.Rad2Deg * angle * i));
+            _bulletManager.SpawnFromPool(baseDescription);
+            yield return new WaitForSeconds(0.05f);
+        }
+        StartCoroutine(WaitAndMarkComplete(new object[] { 1.0f, desc }));
+    }
+
+    public void Move_CrossFireBeams(BossMoveDescription desc)
+    {
+        StartCoroutine(CrossFireBeams(desc));
+    }
+
+    private IEnumerator CrossFireBeams(BossMoveDescription desc)
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            BulletDescription beamDescription = new BulletDescription(BULLET_TYPE.Beam, _boss.transform.position, Quaternion.Euler(0, 0, i * Mathf.PI / 2 * Mathf.Rad2Deg),
+               50, 1.0f, 1.0f, 0.5f, ROTATION_DIRECTION.CounterClockwise, 0.75f);
+            _bulletManager.SpawnFromPool(beamDescription);
+        }
+        yield return new WaitForSeconds(1.0f);
+        for (int i = 0; i < 4; i++)
+        {
+            BulletDescription beamDescription = new BulletDescription(BULLET_TYPE.Beam, _boss.transform.position, Quaternion.Euler(0, 0, i * Mathf.PI / 2 * Mathf.Rad2Deg),
+               50, 1.0f, 1.5f, 0.5f, ROTATION_DIRECTION.Clockwise, 0.75f);
+            _bulletManager.SpawnFromPool(beamDescription);
+        }
+
+        StartCoroutine(WaitAndMarkComplete(new object[] { 2.5f, desc }));
+    }
+
+    public void Move_CircularBlast(BossMoveDescription desc)
+    {
+        float numSlices = 10.0f;
+        float radius = 11.0f;
+        for (int i = 0; i < numSlices; i++)
+        {
+            float angle = i * 2 * Mathf.PI / numSlices;
+            Vector3 bossPosition = _boss.transform.position;
+            BulletDescription plopDescription = new BulletDescription(BULLET_TYPE.Plop,
+                bossPosition + (radius * new Vector3(Mathf.Cos(angle), Mathf.Sin(angle))),
+                Quaternion.identity, 1.0f, 1.0f, 
+                activeDurationS: 1.0f, fadeInDurationS: 0.5f,
+                startScale: 3.0f, endScale: 10.0f);
+            _bulletManager.SpawnFromPool(plopDescription);
+        }
+        StartCoroutine(WaitAndMarkComplete(new object[] { 1.5f, desc }));
+    }
+
+    public void MOVE_BlowUpOnPlayer(BossMoveDescription desc)
+    {
+        StartCoroutine(BlowUpOnPlayer(desc));
+    }
+
+    public IEnumerator BlowUpOnPlayer(BossMoveDescription desc)
+    {
+        int numTimes = 3;
+        for(int i = 0; i < numTimes; i++)
+        {
+            BulletDescription plopDescription = new BulletDescription(BULLET_TYPE.Plop,
+                _player.transform.position,
+                Quaternion.identity, 1.0f, 1.0f, 
+                activeDurationS: 0.75f, fadeInDurationS: 0.5f,
+                startScale: 3.0f, endScale: 15.0f);
+            _bulletManager.SpawnFromPool(plopDescription);
+            if (i + 1 < numTimes)
+            {
+                yield return new WaitForSeconds(2.0f);
+            }
+        }
+        StartCoroutine(WaitAndMarkComplete(new object[] { 1.5f, desc }));
+    }
+
+    public void Move_SpawnFragmentingCirclesAroundBoss(BossMoveDescription desc)
+    {
+        float radius = 15.0f;
+        for (int i = 0; i < 4; i++)
+        {
+            float angle = Mathf.PI / 2 * i;
+            StartCoroutine(SpawnCircleAndExplodeWithBullets(_boss.transform.position + radius * new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0)));
+        }
+        StartCoroutine(WaitAndMarkComplete(new object[] { 2.5f, desc }));
+    }
+
+    private IEnumerator SpawnCircleAndExplodeWithBullets(Vector3 position)
+    {
+        float fadeInDuration = 0.75f;
+        float numFrags = 12.0f;
+        BulletDescription plopDescription = new BulletDescription(BULLET_TYPE.Plop,
+            position, Quaternion.identity, 1.0f, 1.0f,
+            activeDurationS: 1.0f, fadeInDurationS: fadeInDuration,
+            startScale: 3.0f, endScale: 4.0f);
+        _bulletManager.SpawnFromPool(plopDescription);
+
+        yield return new WaitForSeconds(fadeInDuration);
+
+        float angle = Mathf.PI * 2 / numFrags;
+        for(int i = 0; i < numFrags; i++)
+        {
+            BulletDescription baseDescription = new BulletDescription(BULLET_TYPE.Base, position, Quaternion.Euler(0, 0, Mathf.Rad2Deg * angle * i));
+            _bulletManager.SpawnFromPool(baseDescription);
+        }
+    }
+
     private IEnumerator WaitAndMarkComplete(object[] param)
     {
-        yield return new WaitForEndOfFrame();
         float time = (float)param[0];
         BossMoveDescription desc = param[1] as BossMoveDescription;
         yield return new WaitForSeconds(time);
